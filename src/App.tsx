6d082a853717dd97875df388774bc6b948e715d5
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-09-27 00:45:40 +0500

    seperator line

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,23 @@ import Home from './views/Home';
 import Layout from './utlis/ui_components/Layout';
 import ComingSoon from './component/ComingSoon';
 import MyCart from './views/MyCart';
+import { Container } from './utlis';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="comingsoon" element={<ComingSoon />} />
-          <Route path="cart" element={<MyCart />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Container>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="comingsoon" element={<ComingSoon />} />
+            <Route path="cart" element={<MyCart />} />
+          </Route>
+        </Routes>
+      </Router>
+   </Container>
   );
 }
 
